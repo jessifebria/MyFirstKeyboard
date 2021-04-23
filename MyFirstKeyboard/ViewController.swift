e3d070d8639ad341679e4c04e5c585144ed4a722
Jessi Febria <jfebria2302@gmail.com>
2021-04-23 13:24:38 +0700

    starter pack

--- MyFirstKeyboard/ViewController.swift
+++ MyFirstKeyboard/ViewController.swift
@@ -7,13 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    let sentences = ["aaa", "bbb", "ccc"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+    // datasource
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sentences.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tableCell", for: indexPath)
+        
+        cell.textLabel?.text = sentences[indexPath.row]
+        return cell
+    }
+    
+    // delegate
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("tapped")
+    }
 
-
+    override func viewWillAppear(_ animated: Bool) {
+        tableView.reloadData()
+    }
+    
+    
 }
 
